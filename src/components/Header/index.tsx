8e9f36da7b60d0dbf41870303828270011895002
CauaGNP <163431893+CauaGNP@users.noreply.github.com>
2026-02-10 12:12:26 +0000

    fix: Change Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,19 +2,46 @@ import { useEffect, useState } from "react";
 import "./style.css"
 
 export default function Header(){
-    const title = "Cauã Gabriel";
-    const subTitle = "Desenvolvedor Full-Stack"
+    const title = "Bem vindo!!";
+    const subTitle = "Meu nome é Cauã Gabriel, sou desenvolvedor full-stack(React, Nextjs, Spring e Express). Meu objetivo é transformar sonhos em aplicações!!"
 
-    const [displayTitle, setDisplayTitle] = useState("");
-    const [displaySubTitle, setDisplaySubTitle] = useState("");
-    const [index, setIndex] = useState(0);
+    const [displayText, setDisplayText] = useState({
+        title: "",
+        subTitle: ""
+    });
+    const [index, setIndex] = useState({
+        title: 0,
+        subTitle: 0
+    });
 
     useEffect(() => {
-        if(index < title.length){
+        if(index.title < title.length){
             const timeout = setTimeout(() => {
-                setDisplayTitle((prev) => prev+ title[index])
-                setIndex((prev) => prev + 1);
+                setDisplayText((prev) => ({
+                    title: prev.title + title[index.title],
+                    subTitle: prev.subTitle
+                }));
+                setIndex((prev) => ({
+                    title: prev.title + 1,
+                    subTitle: prev.subTitle
+                }));
             }, 100);
+
+            return () => clearTimeout(timeout);
+        }
+
+        if(index.title === title.length && index.subTitle < subTitle.length){
+            const timeout = setTimeout(() =>{
+                setDisplayText((prev) => ({
+                    title: prev.title,
+                    subTitle: prev.subTitle + subTitle[index.subTitle]
+                }))
+
+                setIndex((prev) => ({
+                    title: prev.title,
+                    subTitle: prev.subTitle + 1
+                }))
+            }, 20)
 
             return () => clearTimeout(timeout);
         }
@@ -22,7 +49,12 @@ export default function Header(){
     },[index])
 
     return<header className="headerContain">
-        <h1 className="headerTitle">{displayTitle}</h1>
-        <h2 className="headerSubTitle"></h2>
+        <div className="textHeaderContain">
+            <h1 className="headerTitle">{displayText.title}</h1>
+            <h2 className="headerSubTitle">{displayText.subTitle}</h2>
+        </div>
+        <div className="image">
+
+        </div>
     </header>
 }
