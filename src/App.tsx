bb3166fe09aa170346e2f6ba6b78e1ce50edcca7
CauaGNP <cauagnp13@gmail.com>
2026-02-12 23:28:24 -0300

    Formating code

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,95 @@
-import "./App.css"
-import {Header} from "./components/Header"
-import Person from "./assets/person.png"
-import { Stacks } from "./components/Stacks"
-import { SiJavascript } from "react-icons/si"
+import { SiJavascript } from "react-icons/si";
+import "./App.css";
+import Person from "./assets/person.png";
+import { Header } from "./components/Header";
+import { Stacks } from "./components/Stacks";
 
 function App() {
-
   return (
     <div className="container">
       <Header />
       <main className="main">
-          <section className="aboutMe">
-            <div className="aboutMeTextContain">
-              <h1>Quem sou eu?</h1>
-              <p className="paragraph">Me chamo Cauã Gabriel, sou desenvolvedor Full-Stack com foco no desenvolvimento web e experiência em aplicações mobile. Atualmente, estou no 5º período de Sistemas para Internet, unindo a base acadêmica com a prática na construção de interfaces modernas e backends eficientes.</p>
+        <section className="aboutMe">
+          <div className="aboutMeTextContain">
+            <h1>Quem sou eu?</h1>
+            <p className="paragraph">
+              Me chamo Cauã Gabriel, sou desenvolvedor Full-Stack com foco no
+              desenvolvimento web e experiência em aplicações mobile.
+              Atualmente, estou no 5º período de Sistemas para Internet, unindo
+              a base acadêmica com a prática na construção de interfaces
+              modernas e backends eficientes.
+            </p>
+          </div>
+          <div className="imageContain">
+            <img className="aboutMeImage" src={Person} />
+            <p className="paragrapfImage">
+              Data illustrations by{" "}
+              <a
+                href="https://storyset.com/data"
+                className="storySetLink"
+                target="_blank"
+              >
+                Storyset
+              </a>
+            </p>
+          </div>
+        </section>
+        <section className="stackSection gridLayout">
+          <div className="frontend">
+            <h1>Front-End</h1>
+            <div className="tecnologiesDivExample">
+              <Stacks technologyName="JavaScript">
+                <SiJavascript />
+              </Stacks>
+              <Stacks technologyName="JavaScript">
+                <SiJavascript />
+              </Stacks>
+              <Stacks technologyName="JavaScript">
+                <SiJavascript />
+              </Stacks>
+              <Stacks technologyName="JavaScript">
+                <SiJavascript />
+              </Stacks>
             </div>
-            <img className="aboutMeImage" src={Person}/>
-          </section>
-          <section className="stackSection gridLayout">
-                <div className="frontend">
-                  <h1>Front-End</h1>
-                  <div className="tecnologiesDivExample">
-                      <Stacks technologyName="JavaScript">
-                        <SiJavascript/>
-                      </Stacks>
-                      <Stacks technologyName="JavaScript">
-                        <SiJavascript/>
-                      </Stacks>
-                      <Stacks technologyName="JavaScript">
-                        <SiJavascript/>
-                      </Stacks>
-                      <Stacks technologyName="JavaScript">
-                        <SiJavascript/>
-                      </Stacks>
-                  </div>
-                </div>
-                <div className="backend">
-                   <h1>Front-End</h1>
-                  <div className="tecnologiesDivExample">
-                      <Stacks technologyName="JavaScript">
-                        <SiJavascript/>
-                      </Stacks>
-                      <Stacks technologyName="JavaScript">
-                        <SiJavascript/>
-                      </Stacks>
-                      <Stacks technologyName="JavaScript">
-                        <SiJavascript/>
-                      </Stacks>
-                      <Stacks technologyName="JavaScript">
-                        <SiJavascript/>
-                      </Stacks>
-                  </div>
-                </div>
-              <div className="tools">
-                 <h1>Front-End</h1>
-                  <div className="tecnologiesDivExample">
-                      <Stacks technologyName="JavaScript">
-                        <SiJavascript/>
-                      </Stacks>
-                      <Stacks technologyName="JavaScript">
-                        <SiJavascript/>
-                      </Stacks>
-                      <Stacks technologyName="JavaScript">
-                        <SiJavascript/>
-                      </Stacks>
-                      <Stacks technologyName="JavaScript">
-                        <SiJavascript/>
-                      </Stacks>
-                  </div>
-              </div>
-          </section>
+          </div>
+          <div className="backend">
+            <h1>Back-End</h1>
+            <div className="tecnologiesDivExample">
+              <Stacks technologyName="JavaScript">
+                <SiJavascript />
+              </Stacks>
+              <Stacks technologyName="JavaScript">
+                <SiJavascript />
+              </Stacks>
+              <Stacks technologyName="JavaScript">
+                <SiJavascript />
+              </Stacks>
+              <Stacks technologyName="JavaScript">
+                <SiJavascript />
+              </Stacks>
+            </div>
+          </div>
+          <div className="tools">
+            <h1>Ferramentas</h1>
+            <div className="tecnologiesDivExample">
+              <Stacks technologyName="JavaScript">
+                <SiJavascript />
+              </Stacks>
+              <Stacks technologyName="JavaScript">
+                <SiJavascript />
+              </Stacks>
+              <Stacks technologyName="JavaScript">
+                <SiJavascript />
+              </Stacks>
+              <Stacks technologyName="JavaScript">
+                <SiJavascript />
+              </Stacks>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,62 +1,73 @@
 import { useEffect, useState } from "react";
-import CodeImage from "../../assets/code.png"
-import "./style.css"
+import CodeImage from "../../assets/code.png";
+import "./style.css";
 
-export function Header(){
-    const title = "Bem vindo!!";
-    const subTitle = "Meu nome é Cauã Gabriel, sou desenvolvedor full-stack(React, Nextjs, Spring e Express). Meu objetivo é transformar sonhos em aplicações!!"
+export function Header() {
+  const title = "Bem vindo!!";
+  const subTitle =
+    "Meu nome é Cauã Gabriel, sou desenvolvedor full-stack(React, Nextjs, Spring e Express). Meu objetivo é transformar sonhos em aplicações!!";
 
-    const [displayText, setDisplayText] = useState({
-        title: "",
-        subTitle: ""
-    });
-    const [index, setIndex] = useState({
-        title: 0,
-        subTitle: 0
-    });
+  const [displayText, setDisplayText] = useState({
+    title: "",
+    subTitle: "",
+  });
+  const [index, setIndex] = useState({
+    title: 0,
+    subTitle: 0,
+  });
 
-    useEffect(() => {
-        if(index.title < title.length){
-            const timeout = setTimeout(() => {
-                setDisplayText((prev) => ({
-                    title: prev.title + title[index.title],
-                    subTitle: prev.subTitle
-                }));
-                setIndex((prev) => ({
-                    title: prev.title + 1,
-                    subTitle: prev.subTitle
-                }));
-            }, 100);
+  useEffect(() => {
+    if (index.title < title.length) {
+      const timeout = setTimeout(() => {
+        setDisplayText((prev) => ({
+          title: prev.title + title[index.title],
+          subTitle: prev.subTitle,
+        }));
+        setIndex((prev) => ({
+          title: prev.title + 1,
+          subTitle: prev.subTitle,
+        }));
+      }, 100);
 
-            return () => clearTimeout(timeout);
-        }
+      return () => clearTimeout(timeout);
+    }
 
-        if(index.title === title.length && index.subTitle < subTitle.length){
-            const timeout = setTimeout(() =>{
-                setDisplayText((prev) => ({
-                    title: prev.title,
-                    subTitle: prev.subTitle + subTitle[index.subTitle]
-                }))
+    if (index.title === title.length && index.subTitle < subTitle.length) {
+      const timeout = setTimeout(() => {
+        setDisplayText((prev) => ({
+          title: prev.title,
+          subTitle: prev.subTitle + subTitle[index.subTitle],
+        }));
 
-                setIndex((prev) => ({
-                    title: prev.title,
-                    subTitle: prev.subTitle + 1
-                }))
-            }, 20)
+        setIndex((prev) => ({
+          title: prev.title,
+          subTitle: prev.subTitle + 1,
+        }));
+      }, 20);
 
-            return () => clearTimeout(timeout);
-        }
-        
-    },[index])
+      return () => clearTimeout(timeout);
+    }
+  }, [index]);
 
-    return<header className="headerContain">
-        <div className="textHeaderContain">
-            <h1 className="headerTitle">{displayText.title}</h1>
-            <h2 className="headerSubTitle">{displayText.subTitle}</h2>
-        </div>
-        <div className="imageContain">
-            <img src={CodeImage} className="image"/>
-            <p className="paragrapfImage">Data illustrations by <a href="https://storyset.com/data" className="storySetLink" target="_blank">Storyset</a></p>
-        </div>
+  return (
+    <header className="headerContain">
+      <div className="textHeaderContain">
+        <h1 className="headerTitle">{displayText.title}</h1>
+        <h2 className="headerSubTitle">{displayText.subTitle}</h2>
+      </div>
+      <div className="imageContain">
+        <img src={CodeImage} className="image" />
+        <p className="paragrapfImage">
+          Data illustrations by{" "}
+          <a
+            href="https://storyset.com/data"
+            className="storySetLink"
+            target="_blank"
+          >
+            Storyset
+          </a>
+        </p>
+      </div>
     </header>
+  );
 }
