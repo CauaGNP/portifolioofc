62371bc717e33b4db8aed44cc74e224982a1a630
CauaGNP <163431893+CauaGNP@users.noreply.github.com>
2026-02-19 20:21:24 +0000

    a

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,19 +1,19 @@
 import React, { useState, type ReactNode } from "react";
 
 export function Carousel(children: ReactNode) {
-    const [currentIndex, setCurrentIndex] = useState<number>(0)
+    // const [currentIndex, setCurrentIndex] = useState<number>(0)
 
-    function nexSlide() {
-        setCurrentIndex((prevIndex) =>
-            prevIndex === React.Children.count(children) - 1 ? 0 : prevIndex + 1
-        );
-    };
+    // function nexSlide() {
+    //     setCurrentIndex((prevIndex) =>
+    //         prevIndex === React.Children.count(children) - 1 ? 0 : prevIndex + 1
+    //     );
+    // };
 
-    function prevSlide() {
-        setCurrentIndex((prevIndex) =>
-            prevIndex === 0 ? React.Children.count(children) - 1 : prevIndex - 1
-        );
-    };
+    // function prevSlide() {
+    //     setCurrentIndex((prevIndex) =>
+    //         prevIndex === 0 ? React.Children.count(children) - 1 : prevIndex - 1
+    //     );
+    // };
 
     return <section>
     </section>
